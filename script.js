2f32734242d40be03fb2efde9a14d5135017ef65
pankaj-kb <pankajb2601@gmail.com>
2023-01-22 11:58:23 +0530

    completed project

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-let body = document.querySelector("body")
-let dotDiv = document.createElement("div")
+document.addEventListener('click', function (event) {
 
-document.addEventListener('click', function addFunction(event) {
+    let body = document.querySelector("body")
+    let dotDiv = document.createElement("div")
+    let name = document.querySelector("h1");
+
+    body.appendChild(dotDiv);
+
     let top = event.clientY;
     let left = event.clientX;
-    body.appendChild(dotDiv);
-    dotDiv.classList.add('spot');
+
+    let shapeNum = Math.floor(Math.random() * 3) + 1;
+    console.log(shapeNum);
+
+    if (shapeNum === 1) {
+        dotDiv.classList.add('spot');
+        dotDiv.style.backgroundColor = "blue"
+        name.innerText = "Circle"
+    } else if (shapeNum === 2) {
+        dotDiv.classList.add('triangle');
+        dotDiv.style.color = "red"
+        name.innerText = "Triangle"
+    } else if (shapeNum === 3) {
+        dotDiv.classList.add('square');
+        name.innerText = "Square"
+        dotDiv.style.backgroundColor = "yellow"
+    }
+
     dotDiv.style.top = (top) + 'px'
     dotDiv.style.left = (left) + 'px'
 })
